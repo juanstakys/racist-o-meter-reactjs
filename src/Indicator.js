af1bf35cdb9dcb12dc7e3c878fb949bd3d7a36b0
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-09-11 19:33:05 -0300

    Minimal improvements

--- src/Indicator.js
+++ src/Indicator.js
@@ -1,14 +1,8 @@
 import React from "react";
 
 const Indicator = (props) => {
-    if (props.isItRacist) {
-        return (
-            <p>RACIST!</p>
-        )
-    } else {
-        return (
-            <p>NOT RACIST!</p>
-        )
-    }
+    return(
+        <p>{props.isItRacist ? "RACIST!" : "NOT RACIST!"}</p>
+    )
 };
 export default Indicator;
