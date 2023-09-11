afe2f061ed9fd7a0a181a65cc366c2e78bc08d56
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-09-11 12:09:39 -0300

    API consumption

--- src/Dictaphone.js
+++ src/Dictaphone.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+const RACIST_O_METER_URL = 'http://192.168.0.13:8080/detection'
 
 const Dictaphone = () => {
   const {
+    interimTranscript,
     finalTranscript,
-    listening,
     resetTranscript,
+    listening,
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
+
+  useEffect(() => {
+    if (finalTranscript !== '') {
+      console.log('Final trasncript:', finalTranscript)
+      fetch(RACIST_O_METER_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          statement: `${finalTranscript}`
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch(()=>{console.error("Error al consumir la API")});
+    }
+  }, [interimTranscript, finalTranscript])
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
