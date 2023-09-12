de9f2b9a60c0b6cd660e7911553e5c1523704c34
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-09-12 17:22:43 -0300

    Separate background from container

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ function App() {
   const [explanation, setExplanation] = useState()
 
   // Determine the CSS class based on the isItRacist value
-  const containerClass = isItRacist ? 'red-bg' : 'green-bg';
+  const backgroundClass = isItRacist ? 'red-bg' : 'green-bg';
 
-  return (      
-    <div className={`centered-container ${containerClass}`}>
-      <Indicator isItRacist={isItRacist} explanation={explanation}/>
-      <Dictaphone onAPIResponse={(APIIsItRacist, APIExplanation)=>{
-        setIsItRacist(APIIsItRacist)
-        setExplanation(APIExplanation)
-      }}/>
+  return (
+    <div className={`${backgroundClass}`}>
+      <div className={`centered-container`}>
+        <Indicator isItRacist={isItRacist} explanation={explanation} />
+        <Dictaphone onAPIResponse={(APIIsItRacist, APIExplanation) => {
+          setIsItRacist(APIIsItRacist)
+          setExplanation(APIExplanation)
+        }} />
+      </div>
     </div>
   );
 }
